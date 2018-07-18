517025af1f9a8f39e8940927dd776eb2c494c642
zedonora <timeundtime@gmail.com>
2018-07-19 06:51:35 +0900

    seventh

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
 import React from "react";
+import Movie from "./Movie";
 import {Query} from "react-apollo";
-import {HOME_PAGE} from "./queries"
+import {HOME_PAGE} from "./queries";
 const Home = () => (
     <Query query={HOME_PAGE}>
         {({loading, data, error})=>{
             if(loading) return "loading";
             if(error) return "something happened";
-            return data.movies.map(movie=>(
+            return data.movies.map(movie => (
                 <Movie
-                    id={movie.id}
-                    key={movie.id}
-                    poster={movie.medium_cover_image}
-                    title={movie.title}
-                    rating={movie.rating}
+                  id={movie.id}
+                  key={movie.id}
+                  poster={movie.medium_cover_image}
+                  title={movie.title}
+                  rating={movie.rating}
                 />
             ));
         }}
